f989fe96e43d8556669cb093d8087e2665cc3d8c
ASMAAGT <asmaguetari1@gmail.com>
2023-12-01 10:37:29 +0100

    feature : StudentController

--- src/main/java/com/tp7/clubsmanagement/Controllers/StudentsController.java
+++ src/main/java/com/tp7/clubsmanagement/Controllers/StudentsController.java
@@ -1,4 +1,48 @@
 package com.tp7.clubsmanagement.Controllers;
+import com.tp7.clubsmanagement.Models.Student;
+import com.tp7.clubsmanagement.Services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class StudentsController {
+    @Autowired
+    private StudentService studentService;
+
+
+    @PostMapping("/students")
+    public Student saveStudent(@RequestBody Student student)
+    {
+        return studentService.saveStudent(student);
+    }
+
+    @GetMapping("/Allstudents")
+
+    public List<Student> fetchStudentList()
+    {
+        return studentService.fetchStudentList();
+    }
+    @DeleteMapping("/student/{id}")
+    public String deleteStudentById(@PathVariable("id")
+                                    Integer studentId) {
+        try {
+            studentService.deleteStudentById(studentId);
+            return String.valueOf(new ResponseEntity<>(HttpStatus.NO_CONTENT));
+            //return "Deleted Successfully";
+        } catch (Exception e) {
+            return String.valueOf(new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR));
+        }
+    }
+
+    @PutMapping("/updateStudent/{id}")
+    public ResponseEntity<Student> updateStudent( @RequestBody Student student,@PathVariable Integer studentId) {
+        Student updatedStudent = studentService.updateStudent(student,studentId);
+        return ResponseEntity.ok(updatedStudent);
+    }
+
 }
--- src/main/java/com/tp7/clubsmanagement/Services/StudentService.java
+++ src/main/java/com/tp7/clubsmanagement/Services/StudentService.java
@@ -2,11 +2,13 @@ package com.tp7.clubsmanagement.Services;
 
 import com.tp7.clubsmanagement.Models.Student;
 
+import java.util.List;
+
 public interface StudentService {
     Student saveStudent (Student student);
 
     // read operation
-    Iterable<Student> fetchStudentList();
+    List<Student> fetchStudentList();
 
     // update operation
     Student updateStudent(Student student, Integer studentId);
